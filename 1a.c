9c959bd16241592362e3e3476212b788ae219012
IsaacDsouza <isaacmdsouza@gmail.com>
2024-01-11 18:34:02 +0530

    REVIEW OF FUNDAMENTAL DATA STRUCTURES

--- 1a.c
+++ 1a.c
@@ -5,53 +5,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure for a node in the binary tree
 struct Node {
     int key;
-    struct Node* left;
-    struct Node* right;
+    struct Node *left;
+    struct Node *right;
+
 };
 
-// Function to create a new node with a given key
-struct Node* createNode(int key) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->key = key;
-    newNode->left = newNode->right = NULL;
+struct Node *createNode(int key){
+    struct Node *newNode= (struct Node*)malloc(sizeof(struct Node));
+    newNode->key=key;
+    newNode->left=newNode->right=NULL;
     return newNode;
-}
 
-// Function to search for a key in the binary search tree
-void searchInsert(struct Node** root, int key) {
-    if (*root == NULL) {
+}
+void searchInsert(struct Node** root, int key){
+    if (*root==NULL){
         *root = createNode(key);
-        printf("Key inserted\n");
+        printf("Key inserted \n");
         return;
-    }
 
-    if (key == (*root)->key) {
-        printf("Key found\n");
-    } else if (key < (*root)->key) {
-        searchInsert(&((*root)->left), key);
-    } else {
-        searchInsert(&((*root)->right), key);
+    }
+    if (key==(*root)->key){
+        printf("Key found \n");
+        
+    }else if(key<(*root)->key){
+        searchInsert(&((*root)->left),key);
+    }else {
+        searchInsert(&((*root)->right),key);
+
     }
 }
+int main(){
+    struct Node* root =NULL;
+    int keys[]={50,30,20,40,70,60,80};
+    int numKeys=sizeof(keys)/sizeof(keys[0]);
+    for(int i=0;i<numKeys;i++){
+        searchInsert(&root,keys[i]);
 
-// Example usage
-int main() {
-    struct Node* root = NULL;
 
-    // Insert keys into the binary search tree
-    int keys[] = {50, 30, 20, 40, 70, 60, 80};
-    int numKeys = sizeof(keys) / sizeof(keys[0]);
-
-    for (int i = 0; i < numKeys; i++) {
-        searchInsert(&root, keys[i]);
     }
-
-    // Search for keys in the binary search tree
-    int searchKey = 60;
-    searchInsert(&root, searchKey);
-
+    int searchKey=60;
+    searchInsert(&root,searchKey);
     return 0;
 }
+
+
